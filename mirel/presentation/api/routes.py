b9c5eb63765b2f85fab2741d34b9d1f2d5876560
mihey <daniil53891@yandex.com>
2023-06-06 15:49:39 +0300

    fix

--- mirel/presentation/api/routes.py
+++ mirel/presentation/api/routes.py
@@ -4,7 +4,7 @@ from mirel.config.settings import Settings
 
 settings = Settings()
 
-router = APIRouter(prefix=settings.api_url)
+router = APIRouter(prefix=settings.root_path + settings.api_url)
 router.include_router(product.router, prefix="/products", tags=["products"])
 router.include_router(article.router, prefix="/articles", tags=["articles"])
 router.include_router(image.router, prefix="/images", tags=["images"])
